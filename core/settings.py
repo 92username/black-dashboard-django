122492d2e83e65a7271246f24489ed244e17ec1c
92username <92username@users.noreply.github.com>
2024-12-01 16:49:03 -0300

    fix: database path and add db.sqlite3 to repo

--- core/settings.py
+++ core/settings.py
@@ -96,7 +96,8 @@ else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': 'db.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+
         }
     }
 
